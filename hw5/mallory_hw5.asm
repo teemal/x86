de3d41425f4f269ea142a44c61316d87bc9ed7ca
teemal <taymal1987@gmail.com>
2018-05-10 13:36:06 -0700

    testing stack conversion

--- hw5/mallory_hw5.asm
+++ hw5/mallory_hw5.asm
@@ -1,6 +1,33 @@
+;macro for system write
+	%macro	write_string	2
+	mov	eax,4	;sys write
+	mov	ebx,1	;stdout
+	mov	ecx,%1	;message param
+	mov	edx,%2	;message len param
+	int	80h	;kernel call (interupt)
+	%endmacro
+
+;macro for system read
+	%macro	read_string	2
+	mov	eax,3	;sys read
+	mov	ebx,0	;stdin
+	mov	ecx,%1	;buffer param
+	mov	edx,%2	;buffer size param
+	int	80h
+	%endmacro
+
+;macro to reset registers
+	%macro reset_registers	0
+	mov	eax,0
+	mov	ebx,0
+	mov	ecx,0
+	mov	edx,0
+	%endmacro
+
 section	.bss
-	size:	equ	1024	;const 1024 byte for buffer
+	size:	equ	1024	;const 1024 bits for buffer
 	buffer:	resb	size	;
+
 section	.data
 	user_message	db	'please enter balanced brackets, YA DINGUS!', 0x0a
 			;asks user for input
@@ -10,64 +37,20 @@ section	.data
 			;error message
 	error_len	equ	$-user_error
 			;length of error message
+
 section	.text
 	global _start
 _start:
-	mov	eax,4	;user prompt
-	mov	ebx,1
-	mov	ecx,user_message
-	mov	edx,len_message
-	int	80h	;linux kernel call (interrupt)
+	write_string	user_message,len_message
 
-	mov	eax,3   ;user input
-        mov	ebx,0
-        mov	ecx,buffer
-        mov	edx,1
-        int	80h     ;linux kernel call (interrupt)
+	read_string	buffer,size
 
-	mov	eax,0	;reset registers
+	reset_registers
+
+	mov	cx,'['	;TEST
+	push	cx	;TEST
+	pop	dx	;TEST
+
+	mov	eax,1
 	mov	ebx,0
-	mov	ecx,0
-	mov	edx,0
-
-	push	buffer	;push the buffer onto the stack (little endian)
-	pop	dx	;pop top value into dx
-
-	cmp	dx,'{'	;begin comparisons
-	je	eq_open
-	cmp	dx,'('
-	je	eq_open
-	cmp	dx,'['
-	je	eq_open
-	cmp	dx,'}'
-	je	eq_closed
-	cmp	dx,')'
-	je	eq_closed
-	cmp	dx,']'
-	je	eq_closed	;final comparison
-
-;NEED TO SEE IF STACK IS EMPTY FOR CONTROL FLOW
-;IF NOT EMPTY THEN COMPARE IF EMPTY CHECK EAX AND STDOUT
-
-	mov	eax,1	;sys exit call
-	mov	ebx,0	;return 0
-	int	80h	;kernel call
-
-eq_open:
-	inc	eax	;increment eax by 1
-eq_closed:
-	dec	eax	;decrement eax by 1
-
-zero:			;messages user that brackets are balanced
-	mov	eax,4	;user message
-	mov	ebx,1
-	mov	ecx,user_message
-	mov	edx,len_message
-	int	80h	;linux kernel call
-
-not_zero:		;error message (unbalanced brackets)
-	mov	eax,4	;user prompt
-	mov	ebx,1
-	mov	ecx,user_error
-	mov	edx,error_len
-	int	80h	;kernel call
+	int	80h
